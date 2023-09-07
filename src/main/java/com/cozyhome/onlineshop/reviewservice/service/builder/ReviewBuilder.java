32a9f9be47ecda36d5057338d9d710e039988600
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-09-07 19:00:41 +0200

    CH-414-CH-570 validation and ExceptionHandler jira [validation and ExceptionHandler] https://cozyhometeam.atlassian.net/browse/CH-570?atlOrigin=eyJpIjoiMjU2ZjE3MWVjNjBlNGY2M2E5ZjQyMGFhNzBkMjY1NzYiLCJwIjoiaiJ9

--- src/main/java/com/cozyhome/onlineshop/reviewservice/service/builder/ReviewBuilder.java
+++ src/main/java/com/cozyhome/onlineshop/reviewservice/service/builder/ReviewBuilder.java
@@ -2,9 +2,11 @@ package com.cozyhome.onlineshop.reviewservice.service.builder;
 
 import com.cozyhome.onlineshop.reviewservice.dto.ReviewResponse;
 import com.cozyhome.onlineshop.reviewservice.model.Review;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ReviewBuilder {
     public List<ReviewResponse> buildReviewsResponse(List<Review> reviews) {
         return reviews.stream().map(this::buildReviewResponse).toList();
